2ab94dd4c99497f44bc38f3f1f32541ed1bd1853
torikai-riku <bokkemon528@gmail.com>
2023-06-13 17:56:29 +0900

    up
    敵の上下移動

---
+++ FallingCoin/Assets/enemyMove.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class enemyMove : MonoBehaviour
+{
+    private Vector2 initialPosition;
+
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector2(initialPosition.x,Mathf.Sin(Time.time)*10.0f+initialPosition.y);
+    }
+}
