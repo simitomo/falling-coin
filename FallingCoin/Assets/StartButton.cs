fcb8f4e2e159079e57723a6061fd08d422bb3385
itasi29 <135081567+itasi29@users.noreply.github.com>
2023-08-08 11:53:49 +0900

    up

---
+++ FallingCoin/Assets/StartButton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButton : MonoBehaviour
+{
+    // ボタンが押されたときに動作
+    public void OnClick()
+    {
+        Debug.Log("StartPush");
+    }
+}
